d0e438d40fd3cf361a639a45fd07b2d1b2e6db74
abc7468 <abc7468@naver.com>
2021-08-25 01:00:23 +0900

    트리 순회 : 구현

--- src/Boj/Gold/Q20057.java
+++ src/Boj/Gold/Q20057.java
@@ -7,33 +7,25 @@ public class Q20057 {
     static int N;
     static int[] inOrder;
     static int[] postOrder;
-    static void findPre(int[] in, int[] post){
-        if (post.length==1){
-            System.out.printf("%d ", post[0]);
+    static void findPre(int leftIn, int rightIn, int leftPost, int rightPost){
+        if (rightPost==leftPost){
+            System.out.printf("%d ", postOrder[rightPost]);
             return;
         }
-        if(post.length==0){
+        if(rightPost<leftPost){
             return;
         }
-        System.out.printf("%d ", post[post.length-1]);
-        int len = post.length;
-        int pivot = post[len-1];
+        int pivot = postOrder[rightPost];
+        System.out.printf("%d ", pivot);
         int i;
-        for(i =0; i<len; i++){
-            if(in[i]==pivot){
+        for(i =leftIn; i<rightIn; i++){
+            if(inOrder[i]==pivot){
                 break;
             }
         }
-        int[] leftIn;
-        int[] rightIn;
-        int[] leftPost;
-        int[] rightPost;
-        leftIn = Arrays.copyOfRange(in,0, i);
-        rightIn = Arrays.copyOfRange(in,i+1, len);
-        leftPost = Arrays.copyOfRange(post,0,i);
-        rightPost = Arrays.copyOfRange(post,i,len-1);
-        findPre(leftIn, leftPost);
-        findPre(rightIn, rightPost);
+
+        findPre(leftIn, i-1, leftPost, leftPost + (i-leftIn-1));
+        findPre(i+1, rightIn, leftPost + (i-leftIn), rightPost-1);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -46,6 +38,6 @@ public class Q20057 {
         for(int i =0; i<N; i++){
             postOrder[i] = sc.nextInt();
         }
-        findPre(inOrder, postOrder);
+        findPre(0,N-1,0, N-1);
     }
 }
